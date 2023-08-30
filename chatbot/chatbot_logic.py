9a05cc383ec85906346eaf26aea23bd101d7ae8d
manish gholam <manish.gholam@1finance.co.in>
2023-08-30 13:05:17 +0530

    chatbot logic update

--- chatbot/chatbot_logic.py
+++ chatbot/chatbot_logic.py
@@ -5,11 +5,35 @@ import dotenv
 
 dotenv.load_dotenv()
 
+chat_history = []
+
+import re
+
+def is_query_related_to_history(query, history):
+    """
+    Determine if the query is related to the recent history.
+    """
+    # Extract keywords from the recent history
+    history_keywords = set(re.findall(r'\b\w+\b', history.lower()))
+    
+    # Check if any of the keywords appear in the query
+    for keyword in history_keywords:
+        if keyword in query.lower():
+            return True
+    return False
+
+def get_recent_history_as_context(num_messages=5):
+    """
+    Get the last few messages (both user and bot) as a single string.
+    """
+    recent_history = chat_history[-num_messages:]
+    return "\n".join([f"{sender if sender != 'bot' else ''}: {message}" for sender, message in recent_history])
+
 def construct_index():
     # set maximum input size
-    max_input_size = 500
+    max_input_size = 4000
     # set number of output tokens
-    num_outputs = 500
+    num_outputs = 4000
     # set maximum chunk overlap
     max_chunk_overlap = 20
     # set chunk size limit
@@ -33,10 +57,38 @@ def construct_index():
 
     return index
 
+
+def convert_urls_to_links(text):
+    # Find URLs in the text.
+    url_pattern = re.compile(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
+    # Replace URLs with HTML links.
+    return url_pattern.sub(lambda x: f'<a href="{x.group()}" target="_blank">{x.group()}</a>', text)
+
 def get_response(query):
+    # First, try to get an answer without any context
     if not os.path.exists('index.json'):
         construct_index()
     index = GPTSimpleVectorIndex.load_from_disk('index.json')
-    response = index.query(query)
-    print(response.response)
-    return response.response
+    response = index.query("User: " + query)
+
+    # If the response indicates uncertainty, use chat history as context
+    uncertain_responses = [
+        "I'm sorry, I'm not sure what your question or query is related to",
+        "I don't know"
+    ]
+    if any(phrase in response.response for phrase in uncertain_responses):
+        # Get the recent chat history as context
+        context = get_recent_history_as_context()
+        # If the query is related to history, combine the context with the current user query
+        if is_query_related_to_history(query, context):
+            full_query = context + "\nUser: " + query
+            response = index.query(full_query)
+
+    # Convert URLs in the response to clickable hyperlinks
+    formatted_response = convert_urls_to_links(response.response)
+
+    # Save the interaction to chat history
+    chat_history.append(("user", query))
+    chat_history.append(("bot", formatted_response))
+
+    return formatted_response
